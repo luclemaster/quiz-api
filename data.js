5b00d8c06ac68be56da1db870564e346c8112649
luclemaster <llemaster@email.arizona.edu>
2020-11-30 22:09:01 -0700

    fixed data typo

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
-let q1 = {picture: 'q1.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
-let q2 = {picture: 'q2.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
-let q3 = {picture: 'q3.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
-let q4 = {picture: 'q4.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
-let q5 = {picture: 'q5.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
-let q6 = {picture: 'q6.png', choicess: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q1 = {picture: 'q1.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q2 = {picture: 'q2.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q3 = {picture: 'q3.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q4 = {picture: 'q4.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q5 = {picture: 'q5.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
+let q6 = {picture: 'q6.png', choices: ['c1', 'c2', 'c3'], answer: 'c2'};
 let quiz1 = {name: 'quiz 1', id: 1, picture: 'q1.png', questions: [q1, q2, q3]};
 let quiz2 = {name: 'quiz 2', id: 2, picture: 'q2.png', questions: [q4, q5, q6]};
 let quizzes = [quiz1, quiz2];
